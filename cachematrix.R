74ca6b00a71cbe727f8fbf5e587277d6c131ad6e
Darshan Shah <dsmitshah@gmail.com>
2020-06-12 15:48:38 +0530

    The functions makechachematrix and chachesolve are completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
+## This is the funtion is to chache the inverse of the matrix and then retrieve the inverse through the chache memory
 ## functions do
 
-## Write a short comment describing this function
+## this funtion is to store the inverse in the chache memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL 
+  }
+  get <- function() x
+  setinverse <- function(inverse) m<<-inverse
+  getinverse <- function() m
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function is to retrieve the chache memory
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)){
+      message("Getting the chache memory")
+      return(m)
+    }
+    data <- x$get()
+    x$setinverse
+    m
 }
